045f36d27dfd7f89f49db6eb7926df57714d32ea
Misora Ryo <69433648+MisoraRyo@users.noreply.github.com>
2022-07-15 14:07:31 +0900

    Delete src directory

--- src/vert_star.glsl
+++
@@ -1,9 +0,0 @@
-uniform float uPixelRation;
-uniform vec3 uColor;
-///////////////////////////////////////////////////////////////////////////////////////////////
-void main(){
-
-  gl_PointSize = 3.0 * uPixelRation;//6.0
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-}
